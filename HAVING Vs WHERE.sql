3f2fac3cd6f48b3253b212ab657e69bc75eb729d
Yenuli Obapitiya <146210226+yenuliobapitiya@users.noreply.github.com>
2025-07-09 00:43:29 +0530

    HAVING Vs WHERE Statements

---
+++ HAVING Vs WHERE.sql
@@ -0,0 +1,16 @@
+-- Having Vs Where
+SELECT *
+FROM employee_salary;
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG(age) > 40
+;
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%manager%'
+GROUP BY occupation
+HAVING AVG(salary) > 75000
+;
